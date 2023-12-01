3bce9490eb6ee602a68910f1ee0d2c41632955a0
mozeX <delix.cito@gmail.com>
2023-12-01 14:55:22 +0200

    Initial Commit

---
+++ AncientEgypt/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AncientEgypt
+//
+//  Created by Yurii Derzhylo on 01.12.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
